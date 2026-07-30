072bf3f9d7b4550bc019c5ef0bc57e0e768b05c8
Hafiz-Muhammad-Shayan <hafizshayan284@gmail.com>
2026-07-30 21:52:57 +0500

    9 screens done

--- lib/scrn5.dart
+++ lib/scrn5.dart
@@ -1,3 +1,4 @@
+import 'package:figmadesignui/scrn6.dart';
 import 'package:flutter/material.dart';
 class Scrn5 extends StatefulWidget {
   const Scrn5({super.key});
@@ -11,76 +12,65 @@ class _Scrn5State extends State<Scrn5> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 40),
-              child: Row(
-                children: [
-                  IconButton(
-                      onPressed: (){},
-                      icon: Icon(Icons.arrow_back_ios)),
-                ],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width * 1,
+        height: MediaQuery.of(context).size.height * 1,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40),
+                child: Row(
+                  children: [
+                    IconButton(
+                        onPressed: (){},
+                        icon: Icon(Icons.arrow_back_ios)),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Row(
-                children: [
-                  Text(
-                    "  OTP sent",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Row(
-                children: [
-                  Text(
-                    "   Enter the OTP sent to you",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20,left: 10),
-              child: Row(
-                children: [
-                  Container(
-                    height: 60,
-                    width: 70,
-                    child: Padding(
-                      padding: const EdgeInsets.all(25),
-                      child: TextField(
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Row(
+                  children: [
+                    Text(
+                      "  OTP sent",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF006175),
-                      borderRadius: BorderRadius.circular(10),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  children: [
+                    Text(
+                      "   Enter the OTP sent to you",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Container(
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20,left: 10),
+                child: Row(
+                  children: [
+                    Container(
                       height: 60,
                       width: 70,
                       child: Padding(
                         padding: const EdgeInsets.all(25),
                         child: TextField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                          ),
                           style: TextStyle(
                             color: Colors.white,
                           ),
@@ -91,125 +81,157 @@ class _Scrn5State extends State<Scrn5> {
                         borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Container(
-                      height: 60,
-                      width: 70,
-                      child: Padding(
-                        padding: const EdgeInsets.all(25),
-                        child: TextField(
-                          style: TextStyle(
-                            color: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        height: 60,
+                        width: 70,
+                        child: Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                      ),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF006175),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Container(
-                      height: 60,
-                      width: 70,
-                      child: Padding(
-                        padding: const EdgeInsets.all(25),
-                        child: TextField(
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF006175),
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF006175),
-                        borderRadius: BorderRadius.circular(10),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        height: 60,
+                        width: 70,
+                        child: Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF006175),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        height: 60,
+                        width: 70,
+                        child: Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF006175),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20,left: 10),
-              child: Row(
-                children: [
-                  InkWell(
-                      child: Text(
-                          "Didn't receive any code?",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      )),
-                  InkWell(
-                      child: Text(
-                          " Resend in 01:00",
+              Padding(
+                padding: const EdgeInsets.only(top: 20,left: 10),
+                child: Row(
+                  children: [
+                    InkWell(
+                        child: Text(
+                            "Didn't receive any code?",
                           style: TextStyle(
-                            color: Colors.red,
+                            color: Colors.black,
                             fontSize: 12,
                             fontWeight: FontWeight.w500,
                           ),
-                      )),
-                ],
+                        )),
+                    InkWell(
+                        child: Text(
+                            " Resend in 01:00",
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            ),
+                        )),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 290),
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    overlayColor: Color(0xFF006175),
-                    backgroundColor: Color(0xFF006175),
-                    padding: EdgeInsets.symmetric(horizontal: 140,vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  onPressed: (){
-                    Navigator.push(
-                        context, MaterialPageRoute(
-                      builder: (context) => Scrn5(),));
-                  },
-                  child: Text(
-                    "Next",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 55,top: 30),
-              child: Row(
-                children: [
-                  InkWell(
-                    child: Text(
-                      "Already have an account?",
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w700,
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(top: 0),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      overlayColor: Color(0xFF006175),
+                      backgroundColor: Color(0xFF006175),
+                      padding: EdgeInsets.symmetric(horizontal: 140,vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                    onTap: (){},
-                  ),
-                  Text(
-                    " Sign In",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF006175),
-                    ),
-                  ),
-
-                ],
+                    onPressed: (){
+                      Navigator.push(
+                          context, MaterialPageRoute(
+                        builder: (context) => Scrn6(),));
+                    },
+                    child: Text(
+                      "Next",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 55,top: 30,bottom: 50),
+                child: Row(
+                  children: [
+                    InkWell(
+                      child: Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      onTap: (){},
+                    ),
+                    Text(
+                      " Sign In",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF006175),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
